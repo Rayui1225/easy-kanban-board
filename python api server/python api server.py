89333e64784ed36ad8e2338945f627b2895578c6
rayui <49279418+Rayui1225@users.noreply.github.com>
2024-09-09 22:19:04 +0800

    Update python api server.py

--- python api server/python api server.py
+++ python api server/python api server.py
@@ -11,20 +11,20 @@ app.config['CACHE_TYPE'] = 'simple'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 cache = Cache(app)
 
-# MongoDB 連接設定
+
 client = MongoClient('mongodb://localhost:27017/')
-db = client['kanban']  # 這裡的 'test_database' 是你的數據庫名稱
-Cards = db['Cards']  # 這裡的 'test_collection' 是你的集合名稱
+db = client['kanban'] 
+Cards = db['Cards']  
 Columns = db['Columns'] 
 
-# 插入新文檔到集合的 API
+
 @app.route('/api/add', methods=['POST'])
 def add_data():
     data = request.json
     response = collection.insert_one(data)
     return jsonify({'result': 'success', 'document_id': str(response.inserted_id)})
 
-# 取得所有card資訊
+
 @app.route('/api/Cards', methods=['GET'])
 @cache.cached()
 def get_Cards():
@@ -33,7 +33,7 @@ def get_Cards():
     result = transform_form(result)
     return jsonify(result)
 
-# 取得所有column資訊
+
 @app.route('/api/Columns', methods=['GET'])
 @cache.cached()
 def get_Columns():
@@ -42,7 +42,7 @@ def get_Columns():
     result = transform_form(result)
     return jsonify(result)
 
-# 新增card資訊
+
 @app.route('/api/Cards', methods=['POST'])
 def insert_Cards():
     data = request.json
@@ -50,7 +50,7 @@ def insert_Cards():
     cache.delete('/api/Cards')
     return jsonify({'result': 'success', 'document_id': str(response.inserted_id)})
 
-# 修改column資訊
+
 @app.route('/api/Columns/<id>', methods=['PUT'])
 def update_Columns(id):
     update_data = request.json
@@ -62,7 +62,7 @@ def update_Columns(id):
     else:
         return jsonify({'success': False, 'message': 'No document found with provided ID or no changes made'}), 404
 
-# 修改card資訊
+
 @app.route('/api/Cards/<id>', methods=['PUT'])
 def update_Cards(id):
     update_data = request.json
@@ -73,7 +73,7 @@ def update_Cards(id):
     else:
         return jsonify({'success': False, 'message': 'No document found with provided ID or no changes made'}), 404
 
-# 刪除card資訊
+
 @app.route('/api/Cards/<id>', methods=['DELETE'])
 def delete_Cards(id):
     result = Cards.delete_one({'id': id})
@@ -84,7 +84,6 @@ def delete_Cards(id):
         return jsonify({'success': False, 'message': 'No document found with provided ID'}), 404
 
 def transform_form(documents):
-    # 創建一個新的字典，其中使用每個文檔的 'id' 作為鍵
     result = {doc['id']: {k: v for k, v in doc.items() if k != '_id'} for doc in documents}
     return result
 
